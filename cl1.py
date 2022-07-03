daed48222cab45239798c59f17cfbd964f879a55
Maman <85057664+Xm4nXp@users.noreply.github.com>
2022-07-03 15:17:02 +0700

    Update cl1.py

--- cl1.py
+++ cl1.py
@@ -1,37 +1,58 @@
-#belajar membuat class
-import os,requests
+import requests,json,os,random
+from concurrent.futures import ThreadPoolExecutor
 from rich.console import Console
-aiva = Console()
-os.system("clear")
-class apigithub():
-  def __init__(self,pengguna):
-    self.url = "https://api.github.com/search/repositories"
-    self.params = {
-      'q':f'user:{pengguna}'
-    }
-  def dump(self):
-    r = requests.get(self.url,params=self.params)
-    self.iya = r.json()
-    self.uwu2 = self.iya["items"]
-  def result(self):
-    dde = self.uwu2[0]
-    owner = dde["owner"]
-    dump1 = owner["login"]
-    dump2 = owner["html_url"]
-    dump3 = owner["id"]
-    dump4 = owner["avatar_url"]
-    dump5 = owner["type"]
-    aiva.print(f"Nama : [green bold]{dump1}[/]")
-    aiva.print("Type : ",dump5)
-    aiva.print("Avatar : ",dump4)
-    aiva.print("ID : ",dump3)
-    aiva.print("url : ",dump2)
-    aiva.print("Total Result : ",self.iya["total_count"])
-    aiva.print("Found Repo : ",len(self.uwu2))
-aiva.rule("Api Github Simple")
-aiva.rule("By Xm4nXp")
-pengguna = aiva.input("Masukkan Nama Pengguna : ")
-aiva.rule("Result")
-iya = apigithub(pengguna)
-iya.dump()
-iya.result()
+Aiva = Console()
+class githubmanz():
+  def __init__(self):
+    self.total = []
+    self.count = 0
+    self.count2 = 0
+    self.ra = random.randint(0,255)
+  def tanya(self):
+    os.system("clear")
+    Aiva.print("""╔════════════════════════════════════════════════════════╗
+║ [bold white]Author   :[/] [bold rgb(0,255,255)]Xm4nXp[/]                       [red bold] Github Tools[/]  ║
+║ [bold white]Github   :[/] [bold rgb(0,255,255)][not underline]https://github.com/Xm4nXp[/][/]    ╔══════════════║
+║ [bold white]Version  :[/] [bold rgb(0,255,255)]v1[/]                           ║
+╚════════════════════════════════════════════════════════╝""",style=f"color({self.ra})" )
+    print(""" 1.Search By User\n 2.Search By Query""")
+    try:
+      with ThreadPoolExecutor(max_workers=40) as rahman:
+        tanya = int(Aiva.input(" \nPilih [yellow bold]>[/] "))
+        if tanya == 1:
+          rahman.submit(self.getinfo)
+        elif tanya == 2:
+          rahman.submit(self.getrepo)
+    except ValueError:
+      print("Maman Ganz")
+  def getinfo(self):
+    try:
+      user = Aiva.input("User [yellow bold]>[/] ")
+      url = f"https://api.github.com/users/{user}"
+      r = requests.get(url)
+      iya = r.json()
+      okz = iya["created_at"]
+      if r.status_code == 200:
+        anjaz = [requests.get(url+"/followers").json(),requests.get(url+"/following").json(),requests.get(url+"/repos").json()]
+        repo = anjaz[2][:2:]
+        for mamanganteng in anjaz[2]:
+          self.total.append(mamanganteng["html_url"])
+       #   print(mamanganteng["html_url"])
+        for y in anjaz[0]:
+          iyaz = list(y["login"].split(" "))
+          self.count += len(iyaz)
+          for zxmanz in anjaz[1]:
+             iyaz = list(zxmanz["login"].split(" "))
+             self.count2 += len(iyaz)
+        Aiva.print(f"""\nName           : [green bold]{iya['login']}[/]\nCreate Account : [green bold]{okz}[/]\nFollowers      : [green bold]{self.count} [/]\nFollowing      : [green bold]{self.count2} [/]\nUrl            : [green bold][not underline]{iya["html_url"]}[/] [/]""")
+        Aiva.rule("Repository Found",style="red bold")
+        Aiva.print(f"[green bold]{self.total}[/]")
+        Aiva.rule("akhir",style="red bold")
+      elif r.status_code == 404:
+        print("User Tidak Ditemukan")
+    except KeyError:
+      print("Kesalahan Tidak Diketahui")
+  def getrepo(self):
+      print("MainTenance !")
+if __name__ == "__main__":
+  githubmanz().tanya()
